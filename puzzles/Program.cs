c3fd19a171659881c55d5b431ab0f8351d014acd
Natascha Haberl <nt.haberl@gmail.com>
2021-11-29 22:45:26 -0800

    init commit

---
+++ puzzles/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace puzzles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // RandomArray();
+            TossMultipleCoins();
+        }
+
+        public static int[] RandomArray() {
+            int [] randomArray = new int[10];
+            int min = 5;
+            int max = 25;
+
+            Random randNum = new Random();
+            for (int i = 0; i <= 10; i ++){
+                randomArray[i] = randNum(min, max);
+            }
+            Console.WriteLine(randomArray);
+            return randomArray;
+        }
+
+        public static double TossMultipleCoins(){
+            Console.WriteLine("Tossing a Coin!");
+
+        }
+    }
+}
