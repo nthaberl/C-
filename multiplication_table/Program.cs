f146a6516c664f604ead833f8785996fc5911af1
Natascha Haberl <nt.haberl@gmail.com>
2021-11-29 21:50:55 -0800

    init commit

---
+++ multiplication_table/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace multiplication_table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [,] table = new int[10,10];
+            for (int i = 1; i <= 10; i++){
+                for (int j = 1; j <= 10; j++){
+                    table [i - 1, j - 1] = (i + 1) * (j + 1);
+                    }
+            }
+            Console.WriteLine(table);
+        }
+    }
+}
