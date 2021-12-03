e011e5b7e4e13c7be92060f502c77e34990555ae
Natascha Haberl <nt.haberl@gmail.com>
2021-12-02 23:06:33 -0800

    removed misc files

--- ASP/Razor_Fun/Controllers/HomeController.cs
+++ ASP/Razor_Fun/Controllers/HomeController.cs
@@ -1,25 +1,13 @@
 using Microsoft.AspNetCore.Mvc;
-namespace Portfolio.Controllers     //be sure to use your own project's namespace!
+namespace Razor_Fun.Controllers     //be sure to use your own project's namespace!
 {
     public class HomeController : Controller   //remember inheritance??
     {
         //for each route this controller is to handle:
         [HttpGet("")]       //type of request
-        public ViewResult Index() //ViewResult means we will always render on this route
+        public ViewResult Index()
         {
             return View();
-        }
-
-        [HttpGet("projects")]
-        public string Projects()
-        {
-            return "These are my projects";
-        }
-
-        [HttpGet("contact")]
-        public string Contact()
-        {
-            return "This is my contact!";
         }
     }
 }
