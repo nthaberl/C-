7edd517410357ea34a5e7170a040bc06699b4b1d
Natascha Haberl <nt.haberl@gmail.com>
2021-12-02 19:53:35 -0800

    init commit

---
+++ ASP/Razor_Fun/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+namespace Portfolio.Controllers     //be sure to use your own project's namespace!
+{
+    public class HomeController : Controller   //remember inheritance??
+    {
+        //for each route this controller is to handle:
+        [HttpGet("")]       //type of request
+        public ViewResult Index() //ViewResult means we will always render on this route
+        {
+            return View();
+        }
+
+        [HttpGet("projects")]
+        public string Projects()
+        {
+            return "These are my projects";
+        }
+
+        [HttpGet("contact")]
+        public string Contact()
+        {
+            return "This is my contact!";
+        }
+    }
+}
+
