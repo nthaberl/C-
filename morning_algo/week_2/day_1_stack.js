051f78489034db0898b8df7e41dc778e13dc8d59
Natascha Haberl <nt.haberl@gmail.com>
2021-12-10 22:48:28 -0800

    add Two Stacks  One Queue algo

--- morning_algo/week_2/day_1_stack.js
+++ morning_algo/week_2/day_1_stack.js
@@ -151,3 +151,37 @@ class LinkedListStack {
         return length;
     }
 }
+
+    /**
+ * Class to represent a Queue but is implemented using two stacks to store the
+ * queued items without using any other objects or arrays to store the items.
+ * Retains the FIFO (First in First Out) ordering when adding / removing items.
+ */
+class TwoStackQueue {
+    constructor() {
+        this.stack1 = new Stack(); // as the "true queue" (aka this is where the queue items are generally stored)
+        this.stack2 = new Stack();
+    }
+
+    /**
+     * Adds a new item to the back of the queue.
+     * - Time: O(?).
+     * - Space: O(?).
+     * @param {any} item To be added.
+     * @returns {number} The new number of items in the queue.
+     */
+    enqueue(item) { 
+        this.stack1.items.push(item);
+        return this.stack1.size();
+    }
+
+    /**
+     * Removes the next item in the line / queue.
+     * - Time: O(?).
+     * - Space: O(?).
+     * @returns {any} The removed item.
+     */
+    dequeue() { 
+        
+    }
+}
