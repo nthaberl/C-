91954c10fce0aae670dd5cd3277c4bafc2327344
Natascha Haberl <nt.haberl@gmail.com>
2021-12-22 10:20:30 -0800

    added more functions

--- morning_algo/week_3/binary_search.js
+++ morning_algo/week_3/binary_search.js
@@ -409,7 +409,110 @@ class BinarySearchTree {
             }
             return vals;
         }
-    } 
+
+
+            /**
+     * BFS order: horizontal rows top-down left-to-right.
+     * Converts this BST into an array following Breadth First Search order.
+     * Example on the fullTree var:
+     * [25, 15, 50, 10, 22, 35, 70, 4, 12, 18, 24, 31, 44, 66, 90]
+     * HINT: Use a stack (maybe 2? I honestly don't remember off the top of my head)
+     * jk use a queue
+     * @param {Node} current The current node during the traversal of this tree.
+     * @returns {Array<number>} The data of all nodes in BFS order.
+     */
+    toArrLevelorder(current = this.root) { 
+        let queue = [];
+        let returnVals = [];
+
+        if (current)
+        {
+            queue.push(current);
+        }
+        while (queue.length > 0){
+            let dequeueNode = queue[0];
+            for (let i = 1; i < queue.length; i++){
+                queue[i-1] = queue[i];
+                queue[i] = dequeueNode;
+            }
+
+            queue.pop();
+            returnVals.push(dequeueNode.data);
+
+            if(dequeueNode.left){
+                queue.push(dequeueNode.left)
+            }
+            if(dequeueNode.right){
+                queue.push(dequeueNode.right)
+            }
+        }
+        return returnVals
+    }
+
+    /**
+     * Recursively counts the total number of nodes in this tree.
+     * - Time: O(?).
+     * - Space: O(?).
+     * @param {Node} node The current node during the traversal of this tree.
+     * @returns {number} The total number of nodes.
+     */
+    size(node = this.root, total = 0) { 
+        if (node == null){
+            return 0;
+        }
+        return 1 + this.size(node.left, total) + this.size(node.right, total);
+
+    }
+
+    /**
+     * Calculates the height of the tree which is based on how many nodes from
+     * top to bottom (whichever side is taller).
+     * - Time: O(?).
+     * - Space: O(?).
+     * @param {Node} node The current node during traversal of this tree.
+     * @returns {number} The height of the tree.
+     */
+    height(node = this.root, height = 0) { 
+        if(node == null)
+        {
+            return 0
+        }
+        else
+        {
+            let lHeight = this.height(node.left);
+            let rHeight = this.height(node.right);
+
+            if (lHeight > rHeight)
+                return lHeight + 1;
+                else
+                return rHeight + 1;
+        }
+    }
+
+    /**
+     * Determines if this tree is a full tree. A full tree is a tree where every
+     * node has both a left and a right except for the leaf nodes (last nodes)
+     * - Time: O(?).
+     * - Space: O(?).
+     * @param {Node} node The current node during traversal of this tree.
+     * @returns {boolean} Indicates if this tree is full.
+     */
+    isFull(node = this.root) { 
+        if (node == null){
+            return true;
+        }
+
+    if (node.left == null && node.right == null){
+        return true;
+    }
+
+    if ((node.left != null) && (node.right != null)){
+        return (this.isFull(node.left) && this.isFull(node.right))
+    }
+    return false;
+}
+}
+
     // Left right current
 
 // const emptyTree = new BinarySearchTree();
@@ -475,33 +578,38 @@ const fullTree = new BinarySearchTree();
 // console.log(fullTree.containsRecursive(35));
 // console.log(fullTree.containsRecursive(55));
 // console.log(fullTree.range());
-// fullTree
-//     .insert(25)
-//     .insert(15)
-//     .insert(10)
-//     .insert(22)
-//     .insert(4)
-//     .insert(12)
-//     .insert(18)
-//     .insert(24)
-//     .insert(50)
-//     .insert(35)
-//     .insert(70)
-//     .insert(31)
-//     .insert(44)
-//     .insert(66)
-//     .insert(90);
-// fullTree.print();
-
 fullTree
-.insertRecursive(25)
-.insertRecursive(15)
-.insertRecursive(10)
-.insertRecursive(22)
-.insertRecursive(4)
-.insertRecursive(12)
-.insertRecursive(18)
-.insertRecursive(24)
-.insertRecursive(50)
+    .insert(25)
+    .insert(15)
+    .insert(10)
+    .insert(22)
+    .insert(4)
+    .insert(12)
+    .insert(18)
+    .insert(24)
+    .insert(50)
+    .insert(35)
+    .insert(70)
+    .insert(31)
+    .insert(44)
+    .insert(66)
+    .insert(90);
+fullTree.print();
 
-console.log(fullTree.toArrPostorder());
+// fullTree
+// .insertRecursive(25)
+// .insertRecursive(15)
+// .insertRecursive(10)
+// .insertRecursive(22)
+// .insertRecursive(4)
+// .insertRecursive(12)
+// .insertRecursive(18)
+// .insertRecursive(24)
+// .insertRecursive(50)
+
+// console.log(fullTree.toArrPostorder());
+
+// console.log(fullTree.size());
+// console.log(fullTree.height());
+// console.log(fullTree.isFull());
+console.log(fullTree.toArrLevelorder());
